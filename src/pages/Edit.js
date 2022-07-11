f1c5bbf29418cd782e3be85ae8abba4751fa33a4
ShineTiger <shinetiger.dev@gmail.com>
2022-07-11 20:05:13 +0900

    page moving - useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,4 +1,4 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 // # Query String
 // - Query : 웹 페이지에 데이터를 전달하는 가장 간단한 방법
 // # useSerachParams
@@ -6,6 +6,10 @@ import { useSearchParams } from "react-router-dom";
 // useParams와는 다르게
 // searchParams, setSearchParams 두개로 전달받을 수 있고, 배열형식으로 반환한다.
 const Edit = () => {
+  //# Page moving
+  //페이지를 이동시킬 수 있는 함수를 반환해준다.
+  const navigate = useNavigate();
+
   const [searchParams, setSearchParams] = useSearchParams();
 
   const id = searchParams.get("id");
@@ -20,6 +24,15 @@ const Edit = () => {
       <p>여기는 Edit</p>
       <button onClick={() => setSearchParams({ who: "shinetiger" })}>
         QS 바꾸기
+      </button>
+
+      <button
+        //navigate라는 함수에 인자로 경로값을 전달해주면 useNavigate가 함수navigate를 호출하면서 인자에있던 경로로 옮겨준다.
+        onClick={() => {
+          navigate("/home");
+        }}
+      >
+        홈으로 가기
       </button>
     </div>
   );
